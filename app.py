88eacf91a279b90523c5add7c6f555db999feb34
ihsan-guc <ihsanguc.33@gmail.com>
2020-08-07 02:33:18 +0300

    Updated

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import urllib.request
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://icons8.com/icons/pack/free-icons')
-print(r.status_code)
-soup = BeautifulSoup(r.content,"lxml") 
+req = requests.get('https://icons8.com/icons/pack/free-icons')
+print(req.status_code)
+soup = BeautifulSoup(req.content,"lxml") 
 images = soup.find_all("div",attrs={"class":"icon"})
 i = 1;
 for image in images:
